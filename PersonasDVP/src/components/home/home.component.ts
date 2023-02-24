9149524a9ec7253e3956503e4f32d6670e235334
RobARC <rrondonc@gmail.com>
2023-02-24 13:28:54 -0500

    Update Component Home

--- PersonasDVP/src/components/home/home.component.ts
+++ PersonasDVP/src/components/home/home.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeService } from 'src/services/home.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 export class HomeComponent implements OnInit {
 
   apiList: any;
+  data : any;
+  data2: any
+  map: any;
+  
   constructor(private homeService: HomeService,) {}
   
   ngOnInit(): void {
@@ -21,9 +27,16 @@ export class HomeComponent implements OnInit {
     (await this.homeService.getApi()).subscribe((resp: any) => {
       console.log(resp);
       this.apiList = resp;
-
-      this.apiList = Array.of(this.apiList);
       
+      this.data = Object.values(this.apiList);
+
+      //this.map =  Object.entries(this.data).forEach(([key, value]) => {
+      //  console.log(key)
+      //  console.log(value)
+      //});
+
+    this.data2 = this.data[2];
+
      });
   }
 }
